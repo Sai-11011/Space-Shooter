94fc9508dfd0fc217bfdbc6c9bc353a44c3465b9
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-23 00:37:54 +0530

    Added player movement and bullet spawn

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,24 @@
 extends CharacterBody2D
 
+const SPEED := 300.0
 
-const SPEED = 300.0
+const bullet_scene := preload("res://Scenes/bullet.tscn")
 
-
-func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func process_movement() -> void :
+	var direction := Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED 
+	
+func _physics_process(_delta: float) -> void:
+	process_movement()
+	look_at(get_global_mouse_position())
 	move_and_slide()
+	if Input.is_action_pressed("shoot") and $Timers/BulletTimer.is_stopped() :
+		shoot()
+		$Timers/BulletTimer.start()
+
+func shoot() -> void:
+	var bullet = bullet_scene.instantiate()
+	bullet.global_position=global_position
+	bullet.global_rotation=global_rotation
+	get_parent().add_child(bullet)
+	
