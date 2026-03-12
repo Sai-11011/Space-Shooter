76a4141a22bb9002870c8bc2d1dcc10b50faf431
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-12 16:58:07 +0530

    Options working.

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -6,3 +6,29 @@ extends Control
 func _on_back_button_pressed() -> void:
 	AudioManager.play_click()
 	get_tree().change_scene_to_packed(main_menu)
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	PlayerData.settings.fullscreen = toggled_on
+	
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_master_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	PlayerData.settings.master_volume = value
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	PlayerData.settings.master_volume = value
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Sfx")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	PlayerData.settings.master_volume = value
